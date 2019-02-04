14da011e7b2771a81a7d737b65ff6c9498c66f94
billkabanga <tkbillkabanga@gmail.com>
2019-02-04 20:09:51 +0300

    chore(router): Setup react router version 4 (React-router-dom)
    - Add react-router-dom to App
    - Add home and login components
    - Add home and login view components
    - Create routes file with routes for home and login
    
    [Delivers #163691831]

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
+import Routes from './routes';
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <Routes />
       </div>
     );
   }
