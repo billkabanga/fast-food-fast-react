4684000ca47d57eb30f41e5124a5a2d6c50b833f
billkabanga <tkbillkabanga@gmail.com>
2019-02-05 16:16:55 +0300

    feat(home-page): create home page.
    - Add materialise css to application.
    - Modify home component.
    - Create navbar
    
    [#163722290]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.scss';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min';
 import Routes from './routes';
 
 class App extends Component{
--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './sass/home.scss';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to fast food</h1>
+    <div className="home-page">
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">Fast-Food-Fast</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="#">SignUp</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">About</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div className="container home-body">
+        <h1>Welcome to fast food fast...</h1>
+        <h6>For food delivery service at your convenience.
+        Place your order now and get served instantly.</h6>
+      </div>
     </div>
   );
 };
